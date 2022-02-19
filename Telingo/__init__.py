cc5981f3c1937d5e054dac03eb46b3b43da9f6e1
Brendan <wightmanbrendan@gmail.com>
2022-02-19 18:53:38 -0500

    Initial commit

---
+++ Telingo/__init__.py
@@ -0,0 +1,35 @@
+import os
+
+from flask import Flask, render_template
+from . import auth
+from . import home
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    
+    # 
+    # When ready to deploy https://flask.palletsprojects.com/en/2.0.x/tutorial/deploy/
+    # MAKE SURE TO CHANGE THIS
+    #
+    app.secret_key = 'dev'
+
+    app.config.from_pyfile('config.py', silent=True)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    app.register_blueprint(auth.auth)
+
+    app.register_blueprint(home.home)
+    app.add_url_rule('/', endpoint='index')
+
+    app.register_error_handler(404, page_not_found)
+
+    return app
+
+def page_not_found(e):
+  return render_template('404.html'), 404
