f81f42382fb77b128d813b6f6486576d56730101
BrendanWightman <79772380+BrendanWightman@users.noreply.github.com>
2022-04-11 17:40:33 -0400

    fixed some stuff

--- Telingo/events.py
+++ Telingo/events.py
@@ -9,6 +9,17 @@ userCalls = {} #Dictionary to map UserIDs to their active calls
 def testFunction(data):
     if 'room' in data:
         print("User Joined: " + request.sid + " will be in room " + data['room'])
+    # Delete entry in database
+    roomDb =  Room.query.filter(Room.roomId == session['roomId']).first()
+    emit('transferPage', url_for('msg.landing'), to=roomDb.roomId)
+    if roomDb is not None:
+        print(f"Deleting {roomDb}")
+        if(roomDb.initiator == session['roomId']):
+            roomDb.initiator = None
+        if(roomDb.receiver == session['roomId'] and roomDb.initiator == None):
+            database.session.delete(roomDb)
+        database.session.commit()
+        session['roomId'] = None
         userCalls[request.sid] = data['room']
 
 @socketio.on("disconnect")
@@ -16,7 +27,6 @@ def disconnectFunction():
     print("User Left: " + request.sid)
     if request.sid in userCalls:
         print("Cleaning up " + userCalls[request.sid])
-        emit('transferPage', url_for('msg.landing'), to=session['roomId'])
         del userCalls[request.sid] #delete element from dictionary
 
 @socketio.on("transfer")
@@ -42,15 +52,6 @@ def sendStartMessage(data):
 @socketio.on('leave')
 def on_leave(data):
     room = data['room']
-    # Delete entry in database
-    if room is not None:
-        print(f"Deleting {room}")
-        if(room.initiator == session['roomId']):
-            room.initiator = None
-        if(room.receiver == session['roomId'] and room.initiator == None):
-            database.session.delete(room)
-        database.session.commit()
-        session['roomId'] = None
     leave_room(room)
     print('leaving room: ' + room)
 
